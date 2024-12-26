2a27d0d19ddc20fc6a5de69249b3090decf3e3b5
EhDohWah <ehdohwah007@gmail.com>
2024-12-27 01:22:35 +0700

    Updated vercel.json and index file

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
+// Check if the request is for an asset file
+$requestedAssetFile = __DIR__ . '/../public/import/assets/' . ltrim($_SERVER['REQUEST_URI'], '/');
+if (file_exists($requestedAssetFile) && is_file($requestedAssetFile)) {
+    // Serve the asset file directly
+    return readfile($requestedAssetFile);
+}
+
 require __DIR__ . "/../public/index.php";
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,22 +10,22 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('./import/assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('./import/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="{{ asset('/import/assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ asset('/import/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('/../public/import/assets/vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ asset('/../public/import/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('/../public/import/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('/../public/import/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('/../public/import/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('/../public/import/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('import/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+  <link href="{{ asset('import/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('import/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('import/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('import/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('import/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{ asset('/../public/import/assets/css/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('import/assets/css/style.css') }}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: MyResume
@@ -87,7 +87,7 @@
 
         <div class="row">
           <div class="col-lg-4">
-            <img src="{{ asset('./import/assets/img/profile-img.jpg') }}" class="img-fluid" alt="">
+            <img src="{{ asset('import/assets/img/profile-img.jpg') }}" class="img-fluid" alt="">
           </div>
           <div class="col-lg-8 pt-4 pt-lg-0 content">
             <h3>Junior Programmer &amp; Web Developer.</h3>
