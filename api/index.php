79e99d781fef9b12466e3982c21000da73549ec0
EhDohWah <ehdohwah007@gmail.com>
2024-12-27 11:17:10 +0700

    Updated vercel

--- api/index.php
+++ api/index.php
@@ -1,10 +1,8 @@
 <?php
 
-// Check if the request is for an asset file
-$requestedAssetFile = __DIR__ . '/../public/import/assets/' . ltrim($_SERVER['REQUEST_URI'], '/');
-if (file_exists($requestedAssetFile) && is_file($requestedAssetFile)) {
-    // Serve the asset file directly
-    return readfile($requestedAssetFile);
-}
+/**
+* Here is the serverless function entry
+* for deployment with Vercel.
+*/
 
 require __DIR__ . "/../public/index.php";
